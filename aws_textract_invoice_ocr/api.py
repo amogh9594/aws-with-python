3b49db7d1580f81ad16f426da028c79bf9cea99b
Amogh Kawle <42133366+amogh9594@users.noreply.github.com>
2022-02-17 21:42:20 +0530

    1st update

---
+++ aws_textract_invoice_ocr/api.py
@@ -0,0 +1,64 @@
+import boto3
+import botocore
+from trp import Document
+from textractcaller import call_textract_analyzeid
+
+# S3 Bucket Data
+s3BucketName = "aws-textract-invoice"
+PlaindocumentName = "invoice-1.jpg"
+#FormdocumentName = "Test3.JPG"
+#TabledocumentName = "Test4.JPG"
+
+# Amazon Textract client
+#textractmodule = boto3.client('textract')
+textractmodule = boto3.client("textract", aws_access_key_id="AKIA37LCJSNXPICE7ZLH",
+                              aws_secret_access_key="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", region_name="us-east-1")
+
+#1. PLAINTEXT detection from documents:
+response = textractmodule.detect_document_text(
+    Document={
+        'S3Object': {
+            'Bucket': s3BucketName,
+            'Name': PlaindocumentName
+        }
+    })
+print ('------------- Print Plaintext detected text ------------------------------')
+for item in response["Blocks"]:
+    if item["BlockType"] == "LINE":
+        print ('\033[92m'+item["Text"]+'\033[92m')
+
+
+#2. FORM detection from documents:
+response = textractmodule.analyze_document(
+    Document={
+        'S3Object': {
+            'Bucket': s3BucketName,
+            'Name': PlaindocumentName
+        }
+    },
+    FeatureTypes=["FORMS"])
+doc = Document(response)
+print ('------------- Print Form detected text ------------------------------')
+for page in doc.pages:
+    for field in page.form.fields:
+        print("Key: {}, Value: {}".format(field.key, field.value))
+
+
+
+#2. TABLE data detection from documents:
+response = textractmodule.analyze_document(
+    Document={
+        'S3Object': {
+            'Bucket': s3BucketName,
+            'Name': PlaindocumentName
+        }
+    },
+    FeatureTypes=["TABLES"])
+doc = Document(response)
+print ('------------- Print Table detected text ------------------------------')
+for page in doc.pages:
+    for table in page.tables:
+        for r, row in enumerate(table.rows):
+            itemName  = ""
+            for c, cell in enumerate(row.cells):
+                print("Table[{}][{}] = {}".format(r, c, cell.text))
